048a3fd3b8f820f38e0df2535d0c04c86edf8d97
Subham Karmakar <44800073+SubhamK108@users.noreply.github.com>
2024-06-08 00:28:02 +0530

    Initial commit. 🔨 - Removed boilerplate code - Added & configured next-pwa - Added PWA manifest & app icons - Added fontawesome static files - Added & configured custom local font - Added a 404 page - Configured the Main Layout

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { ReactElement } from "react";
+
+export default function NotFound(): ReactElement {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center text-center font-[sans serif]">
+      <h1 className="text-[2rem] max-sm:text-[1.5rem] mb-5 px-10">{"Sorry, there's nothing at this address."}</h1>
+      <p className="text-4xl mb-10">☹️</p>
+      <Link href="/" replace={true}>
+        <button className="text-3xl max-sm:text-2xl rounded-2xl bg-[#074DA6] dark:bg-[#05336E] hover:bg-[#05346e] dark:hover:bg-[#04234D] hover:ring hover:ring-[#0091ff] dark:hover:ring-[#074DA6] text-gray-200 p-2 h-[4.5rem] w-52 max-sm:h-16 max-sm:w-40">
+          Go to Home
+        </button>
+      </Link>
+    </div>
+  );
+}
---
+++ src/lib/fonts.ts
@@ -0,0 +1,6 @@
+import localFont from "next/font/local";
+
+export const firaCode = localFont({
+  src: "../../public/fonts/FiraCode-VF.woff2",
+  variable: "--font-fira-code"
+});
